d5237adaad19e476dabd423cab80f34f469545ac
surankan de <73996434+surankan-de@users.noreply.github.com>
2022-12-27 14:20:59 +0530

    Update and rename selectionsort.cpp to ssortandbsearch.cpp
    added binary search to selection sort too

--- ssortandbsearch.cpp
+++ ssortandbsearch.cpp
@@ -1,9 +1,10 @@
+// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
     // Write C++ code here
     int ar[10] = {21,32,45,6,12,34,67,100,23,4};
-    
+    int key = 12;
     for (int i = 0;i<9;i++)
     {
         for(int j =i+1 ;j<10;j++)
@@ -16,9 +17,23 @@ int main() {
             }
         }
     }
-    for(int x: ar)
+    
+    int start =0;
+    int end = 9;
+    while(start<=end)
     {
-        cout<<x<<" ";
+        int mid = (start+end)/2;
+        if(ar[mid]==key)
+        {
+            cout<<mid;
+            break;
+        }else if(ar[mid]>key)
+        {
+            end = mid-1;
+        }else
+        {
+            start = mid+1;
+        }
     }
 
     return 0;
