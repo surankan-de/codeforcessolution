13ffc91e1c720620334e848c4a109b3d1128e865
surankan de <73996434+surankan-de@users.noreply.github.com>
2022-12-29 11:57:25 +0530

    Update and rename bubblesort.cpp to bsort&insertsort.cpp

--- bsort&insertsort.cpp
+++ bsort&insertsort.cpp
@@ -24,3 +24,32 @@ int main() {
 
     return 0;
 }
+
+
+//code for insertion sort
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int ar[5] = {23,12,5,67,1};
+    
+    for(int i =1;i<5;i++)
+    {
+        int temp = ar[i];
+        int j =i-1;
+        while(ar[j]>temp&&j>=0)
+        {
+            ar[j+1] = ar[j];
+            j--;
+        }
+        ar[j+1] = temp;
+    }
+    for(int x: ar)
+    {
+        cout<<x<<"\n";
+    }
+    return 0;
+}
