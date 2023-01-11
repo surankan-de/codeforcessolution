00c9226ae306db01efead1d035487e1f88fbde40
surankan de <73996434+surankan-de@users.noreply.github.com>
2023-01-11 20:40:57 +0530

    Update knapsackproblem.cpp

--- knapsackproblem.cpp
+++ knapsackproblem.cpp
@@ -1,48 +1,45 @@
 //knapsack problem maximising profit using greedy algorithm
 
-// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
     // Write C++ code here
-    float bweight = 5;
-    float profit[5] = {10,15,15,7,6};
-    float weight[5] ={2,3,5,7,1};
-    float pwb[5];
-    for(int i =0;i<5;i++)
-    {
-        pwb[i]=profit[i]/weight[i];
-    }
+    float bweight = 15;
+    int n = 7;
+    float profit[n] = {10,5,15,7,6,18,3};
+    float weight[n] ={2,3,5,7,1,4,1};
+    //float pwb[n];
+    
+    
     vector<pair<float,float>> v;
-    for(int i =0;i<5;i++)
+    
+    for(int i =0;i<n;i++)
     {
-        v.push_back({pwb[i],weight[i]});
+        v.push_back({profit[i]/weight[i],weight[i]});
+        
     }
     sort(v.begin(),v.end());
-    float pwbas[5] , wbas[5];
-    for(int i =0;i<5;i++)
+    float pwbas[n] , wbas[n];
+    for(int i =0;i<n;i++)
     {
         pwbas[i] = v[i].first;
         wbas[i] = v[i].second;
     }
-    for(int i =1;i<5;i++)
-    {
-        if (pwbas[i]==pwbas[i-1]&&wbas[i-1]<wbas[i])
-        {
-            swap(wbas[i-1],wbas[i]);
-        }
-    }
+    
+    
+    
+    
     float profit_1 = 0;
-    int index = 4;
+    int index = n-1;
     while(bweight!=0&&index>=0)
     {
         if(bweight>=wbas[index])
         {
-            profit_1+=pwbas[index];
+            profit_1+=pwbas[index]*wbas[index];
             bweight-=wbas[index];
             index--;
         }else{
-            profit_1+=bweight/wbas[index]*pwbas[index];
+            profit_1+=bweight*pwbas[index];
             bweight = 0;
         }
     }
