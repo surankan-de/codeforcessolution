8714644197e5e23d1863af9ddf00ed6327514444
surankan de <73996434+surankan-de@users.noreply.github.com>
2023-06-11 10:46:51 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,10 +1,17 @@
 // Online C compiler to run C program online
 #include <stdio.h>
+#include<stdlib.h>
 typedef struct lol{
         int x;
         struct lol *next;
     }node;  
-
+void addhead(node **head,int y)
+{
+    node *addh = (node *)malloc(sizeof(node));
+    addh->x = y;
+    addh->next = *head;
+    *head = addh;
+}
 void add (node *head,int y)
 {
    node * adde = (node *)malloc(sizeof(node)) ;
@@ -20,6 +27,23 @@ void add (node *head,int y)
    
    ptr->next = adde;
    
+}
+void addpos(node **head,int pos,int y)
+{
+    node *ptr = (node *)malloc(sizeof(node));
+    ptr = *head;
+    int count =0;
+    for(count =0;count<pos;count++)
+    {
+        ptr=ptr->next;
+    }
+    node * addpos = (node *)malloc(sizeof(node));
+    addpos->x = y;
+    addpos->next = ptr->next;
+    
+    ptr->next = addpos;
+    
+    
 }
 
 int main() {
@@ -49,8 +73,26 @@ int main() {
         }
     }
     add(head,6);
+    addhead(&head,7);
     
-    for(int i =0;i<6;i++)
+    int count =0;
+    tmp =head;
+    while(tmp->next!=NULL)
+    {
+        count++;
+        tmp=tmp->next;
+    }
+    int pos=0,value =0;
+    printf("enter pos and value: ");
+    
+    scanf("%d %d",&pos,&value);
+    if(pos<=count-1)
+    {
+        addpos(&head,pos,value);
+    }
+    
+    
+    for(int i =0;i<8;i++)
     {
         if(i==0)
         {
